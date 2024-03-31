ba35495c2ba92598bcbeaa90285c9fa0d3794bd3
Laptop Piotrusia <skowronpiotrmail@gmail.com>
2024-03-31 20:46:45 +0200

    Creating Skill model

--- virtual_resume/main/models.py
+++ virtual_resume/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_lenght=20, blank=True, null=True)
+    score = models.IntegerField(default=80, blank=True, null=True)
+    image = models.FileField(upload_to="skills", blank=True, null=True)
+    is_key_skill = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Skills"
+        verbose_name = "Skill"
+
+    def __str__(self) -> str:
+        return self.name
